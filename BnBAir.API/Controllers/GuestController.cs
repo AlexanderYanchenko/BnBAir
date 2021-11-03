90aa672dc98d597bc30507277fce7d7564b4f9eb
Alexander <Alexander.yanchenko31@gmail.com>
2021-11-03 14:59:45 +0200

    feat: Add Guest Controller with Get Requests

---
+++ BnBAir.API/Controllers/GuestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using BnBAir.BLL.DTO;
+using BnBAir.BLL.Interfaces;
+using BnBAir.DAL.Enitities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BnBAir.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestController : ControllerBase
+    {
+        private readonly IService<GuestDTO> _guestService;
+
+        public GuestController(IService<GuestDTO> guestService)
+        {
+            _guestService = guestService;
+        }
+
+        [HttpGet]
+        public IActionResult GetGuests()
+        {
+            return Ok(_guestService.Get().ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGuestById(Guid id)
+        {
+            return Ok(_guestService.GetById(id));
+        }
+    }
+}
