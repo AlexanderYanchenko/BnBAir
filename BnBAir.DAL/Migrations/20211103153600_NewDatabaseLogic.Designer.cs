e4c96667a6b7c9988ccb6c4c859b4d03b1926f74
Alexander <Alexander.yanchenko31@gmail.com>
2021-11-03 18:05:33 +0200

    feat: add new Migration

--- BnBAir.DAL/Migrations/20211103153600_NewDatabaseLogic.Designer.cs
+++ BnBAir.DAL/Migrations/20211103153600_NewDatabaseLogic.Designer.cs
@@ -4,14 +4,16 @@ using BnBAir.DAL.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BnBAir.DAL.Migrations
 {
     [DbContext(typeof(ReservationContext))]
-    partial class ReservationContextModelSnapshot : ModelSnapshot
+    [Migration("20211103153600_NewDatabaseLogic")]
+    partial class NewDatabaseLogic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
