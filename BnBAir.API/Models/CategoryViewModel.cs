746ff8eca006e7c1f12c9fd7b8830d603b694f50
Alexander <Alexander.yanchenko31@gmail.com>
2021-11-04 00:27:23 +0200

    fix : fix repo

---
+++ BnBAir.API/Models/CategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using BnBAir.DAL.Enitities;
+
+namespace BnBAir.API.Models
+{
+    public class CategoryViewModel
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public int Bed { get; set; }
+
+        public virtual ICollection<Room> Rooms { get; set; }
+
+        public virtual ICollection<CategoryDate> CategoryDates { get; set; }
+
+    }
+}
---
+++ BnBAir.BLL/Interfaces/IServiceUW.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BnBAir.BLL.DTO;
+
+namespace BnBAir.BLL.Interfaces
+{
+    public interface IServiceUW : IDisposable
+    {
+        IService<CategoryDateDTO> CategoryDatesDTO { get; }
+        IService<CategoryDTO> CategoriesDTO { get; }
+        IService<GuestDTO> GuestsDTO { get; }
+        IService<ReservationDTO> ReservationsDTO { get; }
+        IService<RoomDTO> RoomsDTO { get; }
+    }
+}
