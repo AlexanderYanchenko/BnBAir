5e68fe3ddfb33e412364c347f5e73caac2187761
Alexander <Alexander.yanchenko31@gmail.com>
2021-11-06 03:52:08 +0200

    feat: add monitor bookings for Admin

---
+++ BnBAir.API/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using BnBAir.BLL.DTO;
+using BnBAir.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BnBAir.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private IService<ReservationDTO> _reservations;
+
+        public AdminController(IService<ReservationDTO> reservations)
+        {
+            _reservations = reservations;
+        }
+
+        [HttpGet("monitoring")]
+        public IActionResult MonitorBooking()
+        {
+            return Ok(_reservations.Get());
+        }
+    }
+}
